9f0d6a91c0b9d7fe5f76946d73915845ccd8fe45
Paul_Barzallo <barzallopa@gmail.com>
2019-02-24 02:29:56 +0100

    actualizacion de validate.js

--- web/js/validate.js
+++ web/js/validate.js
@@ -1,43 +1,100 @@
 function addValidatesAccess() {
 	$('#signup input[type="password"]').blur(validarPasswords);
 	$('#email_signup').blur(validarEmail);
-	$('#age_signup').blur(validarEdad);
+	$('#btn_signup').click(submitSignup);
 }
-
+function validarUser() {
+	var user = $('#user_signup');
+	if (user.val() == ""){
+		user.parent().next().append(makeAlert("alert-danger","El usuario no puede estar vacío", true));
+		return false;
+	}
+	return true;
+}
 function validarPasswords() {
 	var pwd1 = $(this);
 	var pwd2;
+
+	if ($('#pwd_signup_1').val() != ""){
+		$('#pwd_signup_1').parent().next().empty();
+		$('#pwd_signup_2').parent().next().empty();
+	}
+	
+
+	if (pwd1.attr("id") == undefined){
+		pwd1 = $('#pwd_signup_1');
+		pwd2 = $('#pwd_signup_2');
+		if (pwd1.val() == "" && pwd2.val() == "") {
+			pwd1.parent().next().append(makeAlert("alert-danger","La contraseña no puede estar vacía", true));
+			return false;
+		}
+	}
+	
 	if (pwd1.attr("id").endsWith("1")) pwd2 = $("#pwd_signup_2");
 	else pwd2 = $("#pwd_signup_1");
 
-	pwd1.parent().next().empty();
-	pwd2.parent().next().empty();
-
 	if (pwd1.val() != pwd2.val() && pwd1.val() != "" && pwd2.val() != ""){
-		pwd1.parent().next().append(makeAlert("alert-danger","Las contraseñas no coinciden", false));
+		pwd1.parent().next().append(makeAlert("alert-danger","Las contraseñas no coinciden", true));
+		return false;
 	}
+	return true;
 }
 function validarEmail() {
-	var email = $(this);
-	email.parent().next().empty();
+	var email = $('#email_signup');
+	if (email.val() != "") email.parent().next().empty();
+
+	if ($(this).attr("id") == undefined && email.val() == ""){
+		email.parent().next().append(makeAlert("alert-danger","El email no puede estar vacío", true));
+		return false;
+	}
 	var regular_expression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //expresión regular para validar la estructura del email
 	if (!regular_expression.test(email.val()) && email.val() != ""){
-		email.parent().next().append(makeAlert("alert-danger","El email no es correcto", false));
+		email.parent().next().append(makeAlert("alert-danger","El email no es correcto", true));
+		return false;
 	}
+	return true;
 }
 function validarEdad() {
-	var edad = $(this);
-	email.parent().next().empty();
+	var edad = $("#age_signup");
+	edad.parent().next().empty();
 	if (edad.val() == 0){
-		email.parent().next().append(makeAlert("alert-danger","El email no es correcto", false));
+		edad.parent().next().append(makeAlert("alert-danger","Este campo es obligatorio", true));
+		return false;
 	}
+	return true;
 }
 function validarBarrio() {
-	var barrio = $(this);
+	var barrio = $("#distric_signup");
+	barrio.parent().next().empty();
+	if (barrio.val() == "---Barrio---"){
+		barrio.parent().next().append(makeAlert("alert-danger","Este campo es obligatorio", true));
+		return false;
+	}
+	return true;
 }
 function validarHobbies() {
-	var hobbies = $(this);
+	var hobbies = $("#hobbies_signup");
+	var count = 0;
+	hobbies.parent().next().empty();
+
+	var checks = $('input[name="hobbies_signup"]');
+	$.each(checks, function(i, value) {
+		if ($(value).is(':checked')) count+=1;
+	});
+
+	if (count == 0){
+		hobbies.parent().next().append(makeAlert("alert-danger","Este campo es obligatorio", true));
+		return false;
+	}
+	return true;
 }
 function submitSignup() {
-	$("#form_signup").subit();
+	count = 0;
+	if (!validarUser()) count+=1;
+	if (!validarPasswords()) count+=1;
+	if (!validarEmail()) count+=1;
+	if (!validarEdad()) count+=1;
+	if (!validarBarrio()) count+=1;
+	if (!validarHobbies()) count+=1;
+	if (count == 0) $("#form_signup").submit();
 }
